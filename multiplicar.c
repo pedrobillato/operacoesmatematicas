cbe18f993125cae0e3b134c15e02982ed781cbd1
pedrobillato <pedrobillatoledo0@gmail.com>
2025-12-02 20:05:05 -0300

    Update multiplicar.c

--- multiplicar.c
+++ multiplicar.c
@@ -1,4 +1,18 @@
+int multiplicar(int numero1,int numero2)
+{
+    int contador1,contador2,produto;
 
-int multiplicar(int a, int b) {
-    return a * b;
+    contador1 = 0;
+    contador2 = 0;
+    produto = 0;
+
+    while (contador1 < numero1){
+        contador2 = 0;
+        while (contador2 < numero2){
+            produto++; // produto = produto + 1;
+            contador2++;   
+        }
+        contador1++;    
+    }
+   return produto;
 }
