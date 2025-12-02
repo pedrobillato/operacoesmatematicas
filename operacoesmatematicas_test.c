4683f5c2e3794fa8ecd2a890044f65b28afae0ed
pedrobillato <pedrobillatoledo0@gmail.com>
2025-12-02 20:05:39 -0300

    Update operacoesmatematicas_test.c

--- operacoesmatematicas_test.c
+++ operacoesmatematicas_test.c
@@ -6,11 +6,11 @@
 int main() {
    
     assert(multiplicar(2, 3) == 6);
-    assert(multiplicar(-4, 5) == -20);
+    assert(multiplicar(4, 5) == 20);
 
     
     assert(dividir(10, 2) == 5);
-    assert(dividir(-9, 3) == -3);
+    assert(dividir(9, 3) == 3);
 
     return 0;
 }
